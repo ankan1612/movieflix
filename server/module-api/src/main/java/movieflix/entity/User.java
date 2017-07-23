0326b2fab87562caf94875dbc43b8e4306889e3d
Ankan Kumar Giri <agiri000@citymail.cuny.edu>
2017-07-23 17:01:55 -0400

    feat(module-api): add user entity

---
+++ server/module-api/src/main/java/movieflix/entity/User.java
@@ -0,0 +1,77 @@
+package movieflix.entity;
+
+import com.sun.istack.internal.NotNull;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by Ankan on 7/23/2017.
+ */
+@Entity
+@Table
+public class User {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Id
+    @GenericGenerator(name="customUUID", strategy="uuid2")
+    @GeneratedValue(generator = "customUUID")
+    private  String id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String role;
+}
