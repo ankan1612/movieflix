1f852fee0371c3ac5df9e0d86702aafc87f7c93f
Ankan Kumar Giri <agiri000@citymail.cuny.edu>
2017-07-23 05:08:20 -0400

    feat(module-api): add config and initializer files

---
+++ server/module-api/src/main/java/movieflix/service/WebConfig.java
@@ -0,0 +1,14 @@
+package movieflix.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by Ankan on 7/23/2017.
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebConfig {
+}
---
+++ server/module-api/src/main/java/movieflix/service/WebInitializer.java
@@ -0,0 +1,24 @@
+package movieflix.service;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by Ankan on 7/23/2017.
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/api/*" };
+    }
+}
